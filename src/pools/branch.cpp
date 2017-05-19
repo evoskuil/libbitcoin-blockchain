2ce85dff7460c536638ff36ee6e111f2c865da8a
evoskuil <eric@voskuil.org>
2017-05-19 14:21:05 -0700

    Adapt to move of proof from block to header.

--- src/pools/branch.cpp
+++ src/pools/branch.cpp
@@ -134,7 +134,7 @@ uint256_t branch::work() const
 
     // Not using accumulator here avoids repeated copying of uint256 object.
     for (auto block: *blocks_)
-        total += block->proof();
+        total += block->header().proof();
 
     return total;
 }
